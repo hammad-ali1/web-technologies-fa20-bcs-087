6a83813252acc7da749d53ad4b04db1e3dba5447
Hammad Ali <hammad.a22002@gmail.com>
2023-03-05 00:40:48 +0500

    [LP][ADD] hamburger animation

--- labs/landing_page/app/js/script.js
+++ labs/landing_page/app/js/script.js
@@ -1,1 +1,8 @@
-console.log("test");
+//animate hamburger icon
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+
+hamburgerBtn.addEventListener("click", function () {
+  if (hamburgerBtn.classList.contains("open"))
+    hamburgerBtn.classList.remove("open");
+  else hamburgerBtn.classList.add("open");
+});
