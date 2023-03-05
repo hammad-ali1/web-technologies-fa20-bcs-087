2870e68171e47aa631b2cb14bf80dc97a82eacc3
Hammad Ali <hammad.a22002@gmail.com>
2023-03-05 13:05:39 +0500

    [LP][ADD] stop scrolling when menu open

--- labs/landing_page/app/js/script.js
+++ labs/landing_page/app/js/script.js
@@ -1,5 +1,6 @@
 //animate hamburger icon
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector(".header__menu");
@@ -7,12 +8,14 @@ const fadingElements = document.querySelectorAll(".has-fade");
 hamburgerBtn.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    body.classList.remove("no-scroll");
     fadingElements.forEach((elem) => {
       elem.classList.add("fade-out");
       elem.classList.remove("fade-in");
     });
   } else {
     header.classList.add("open");
+    body.classList.add("no-scroll");
     fadingElements.forEach((elem) => {
       elem.classList.add("fade-in");
       elem.classList.remove("fade-out");
