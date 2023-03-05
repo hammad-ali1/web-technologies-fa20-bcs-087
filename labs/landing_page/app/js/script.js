b90a0a02847557f3bccee6a3fecee61e39c36cda
Hammad Ali <hammad.a22002@gmail.com>
2023-03-05 12:56:37 +0500

    [LP][EDIT] fading menu

--- labs/landing_page/app/js/script.js
+++ labs/landing_page/app/js/script.js
@@ -2,14 +2,20 @@
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const menu = document.querySelector(".header__menu");
+const fadingElements = document.querySelectorAll(".has-fade");
 hamburgerBtn.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadingElements.forEach((elem) => {
+      elem.classList.add("fade-out");
+      elem.classList.remove("fade-in");
+    });
   } else {
     header.classList.add("open");
-    overlay.classList.add("fade-in");
-    overlay.classList.remove("fade-out");
+    fadingElements.forEach((elem) => {
+      elem.classList.add("fade-in");
+      elem.classList.remove("fade-out");
+    });
   }
 });
