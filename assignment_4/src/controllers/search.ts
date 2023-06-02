80a6b961e49054c31e350f296a66a32aafae9235
Hammad Ali <hammad.a22002@gmail.com>
2023-06-02 14:41:44 +0500

    search shows functionality

--- assignment_4/src/controllers/search.ts
+++ assignment_4/src/controllers/search.ts
@@ -3,9 +3,18 @@ import api from "@api/moviedb";
 
 export const resolveQuery = asyncHandler(async (req, res) => {
   const queryParams = req.query.query;
+  const searchType = req.query.searchType;
+
   if (typeof queryParams === "string") {
     const movies = await api.searchMovies(queryParams, 1);
-    res.render("search", { movies });
+    const shows = await api.searchShows(queryParams, 1);
+
+    res.render("search", {
+      movies,
+      shows,
+      searchTerm: queryParams,
+      searchType,
+    });
   } else {
     res.render("error");
   }
