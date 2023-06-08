b6cb8a25410f43018ac725f9350bef10ac9a9ed9
Hammad Ali <hammad.a22002@gmail.com>
2023-06-08 15:44:42 +0500

    complete api

--- lab_terminal/src/routers/VoltageReadings.routes.ts
+++ lab_terminal/src/routers/VoltageReadings.routes.ts
@@ -9,6 +9,17 @@ VoltageReadingsRouter.get("/", async (req, res) => {
   res.json(voltageReadings);
 });
 
+VoltageReadingsRouter.get("/:id", async (req, res) => {
+  try {
+    const voltageReadings = await VoltageReading.findById(req.params.id);
+    if (voltageReadings) {
+      res.json(voltageReadings);
+    } else {
+      res.status(404).json({ error: "Voltage Reading Not Found" });
+    }
+  } catch (err) {}
+});
+
 VoltageReadingsRouter.post("/", async (req, res) => {
   try {
     await voltageReadingSchema.validateAsync(req.body);
@@ -35,6 +46,31 @@ VoltageReadingsRouter.delete("/:id", async (req, res) => {
     );
   } catch (err) {
     res.status(500).json({ error: err, message: "Server Error" });
+  }
+});
+
+VoltageReadingsRouter.put("/:id", async (req, res) => {
+  try {
+    const voltageReading = await VoltageReading.findById(req.params.id);
+    if (voltageReading) {
+      await voltageReadingSchema.validateAsync(req.body);
+      voltageReading.max = req.body.max;
+      voltageReading.min = req.body.min;
+      voltageReading.avg = req.body.avg;
+      await voltageReading.save();
+      res.json(voltageReading);
+    } else {
+      res.status(404).json({ error: "Voltage Reading Not Found" });
+    }
+  } catch (err) {
+    if (Joi.isError(err)) {
+      res.json({
+        error: err.details[0].message,
+        message: "Invalid Voltage Reading Data",
+      });
+    } else {
+      res.status(500).json({ error: err, message: "Server Error" });
+    }
   }
 });
 export default VoltageReadingsRouter;
