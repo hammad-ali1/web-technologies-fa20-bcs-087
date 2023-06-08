653b4459db22fa435714a942ecfa4135aa9b2752
Hammad Ali <hammad.a22002@gmail.com>
2023-06-08 15:08:58 +0500

    add post/get routes

--- lab_terminal/src/routers/VoltageReadings.routes.ts
+++ lab_terminal/src/routers/VoltageReadings.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import VoltageReading from "@models/VoltageReading";
 import Joi from "joi";
+import { voltageReadingSchema } from "@validations/VoltageReading";
 const VoltageReadingsRouter = Router();
 
 VoltageReadingsRouter.get("/", async (req, res) => {
@@ -10,12 +11,17 @@ VoltageReadingsRouter.get("/", async (req, res) => {
 
 VoltageReadingsRouter.post("/", async (req, res) => {
   try {
+    await voltageReadingSchema.validateAsync(req.body);
     const voltageReadings = new VoltageReading(req.body);
+
     await voltageReadings.save();
     res.json(voltageReadings);
   } catch (err) {
     if (Joi.isError(err)) {
-      res.json({ error: err, message: "Invalid Voltage Reading Data" });
+      res.json({
+        error: err.details[0].message,
+        message: "Invalid Voltage Reading Data",
+      });
     } else {
       res.json({ error: err, message: "Server Error" });
     }
