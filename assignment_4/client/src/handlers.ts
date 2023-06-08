b5d66b3b8e9ddd2ae0abf30e81d424c8dc08b21c
Hammad Ali <hammad.a22002@gmail.com>
2023-06-08 20:22:36 +0500

    add favorite movie logic to client side

--- assignment_4/client/src/handlers.ts
+++ assignment_4/client/src/handlers.ts
@@ -1,16 +1,48 @@
 function favMovieHandler() {
-  $(document).on("click", ".fav-icon", function (e) {
+  $(document).on("submit", "#addToFavForm", function (e) {
     e.stopPropagation();
-    $(this).children("i").toggleClass("fa-solid fa-regular");
+    e.preventDefault();
+    const url = $(this).attr("action");
+    const method = $(this).attr("method");
+    const isFavorite = $(this).find("i").hasClass("fa-solid");
+    $.ajax({
+      url,
+      method,
+      success: function () {
+        if (isFavorite)
+          displayFlashMsg("Movie removed from favorites", "success");
+        else displayFlashMsg("Movie added to favorites", "success");
+      },
+      error: function () {
+        displayFlashMsg("Error occured", "error");
+      },
+    });
+    $(this).find("i").toggleClass("fa-regular fa-solid");
   });
 }
 
 function flashMsgsAnimation() {
   // Slide down and fade in
-  $(".alert")
+  slideDownUpAnimation($(".alert"));
+}
+
+function displayFlashMsg(message: string, type: "success" | "error") {
+  const flashMsgs = $(".flash-msgs");
+  if (type === "success") {
+    flashMsgs.append(`<div class="alert alert-success">${message}</div>`);
+    slideDownUpAnimation(flashMsgs.children(".alert"));
+  }
+}
+
+function slideDownUpAnimation(element: JQuery<HTMLElement>) {
+  $(element)
     .hide()
     .slideDown(500, function () {
-      $(this).delay(1000).slideUp(500);
+      $(this)
+        .delay(1000)
+        .slideUp(500, function () {
+          $(this).remove();
+        });
     });
 }
 
